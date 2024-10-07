dbc46157a61ab979829da0190106a27f64cb2c4e
searchmoon <moon901212@gmail.com>
2024-10-07 23:56:29 +0900

    알고리즘 문제풀이

--- algoCamp/21-30.js
+++ algoCamp/21-30.js
@@ -61,86 +61,111 @@ function solution(v) {
 
 // 풀이2. 객체를 이용한 풀이
 function solution(v) {
-    const xMap = {};
-    const yMap = {};
+  const xMap = {};
+  const yMap = {};
 
-    for (let i = 0; i < v.length; i++) {
-        xMap[v[i][0]] = (xMap[v[i][0]] || 0) + 1;
-        yMap[v[i][1]] = (yMap[v[i][1]] || 0) + 1;
-    }
+  for (let i = 0; i < v.length; i++) {
+    xMap[v[i][0]] = (xMap[v[i][0]] || 0) + 1;
+    yMap[v[i][1]] = (yMap[v[i][1]] || 0) + 1;
+  }
 
-    const x = Object.keys(xMap).find(key => xMap[key] === 1);
-    const y = Object.keys(yMap).find(key => yMap[key] === 1);
+  const x = Object.keys(xMap).find((key) => xMap[key] === 1);
+  const y = Object.keys(yMap).find((key) => yMap[key] === 1);
 
-    return [Number(x), Number(y)];
+  return [Number(x), Number(y)];
 }
 
 // 문제 24. 나누어 떨어지는 숫자 배열
 // https://school.programmers.co.kr/learn/courses/30/lessons/12910
 //풀이. filter를 이용한 풀이
-function solution(arr, divisor){
-    let answer = arr.filter(int => int % divisor === 0);
-    return answer.length === 0 ? [-1]: answer.sort((a, b) => a - b);
+function solution(arr, divisor) {
+  let answer = arr.filter((int) => int % divisor === 0);
+  return answer.length === 0 ? [-1] : answer.sort((a, b) => a - b);
 }
 
 // 문제 25. 없는 숫자 더하기
 // https://school.programmers.co.kr/learn/courses/30/lessons/86051
 // 풀이
 
-function solution(numbers){
-    let answer = 0;
-    // 이부분은
-    let arr = [];
-    
-    for(let i = 0; i < 10; i++){
-        arr.push(i);
-    }
-    // 1. const arr = new Array(10).fill().map((_, i) => i);
-    // 2. const arr = [...Array(10).keys()]; 
-    // 이렇게 더 간단하게 작성할 수 도 있다.
+function solution(numbers) {
+  let answer = 0;
+  // 이부분은
+  let arr = [];
 
-    for(let num of arr){
-        if(!numbers.includes(num)){
-            answer += num;
-        }
-    }
+  for (let i = 0; i < 10; i++) {
+    arr.push(i);
+  }
+  // 1. const arr = new Array(10).fill().map((_, i) => i);
+  // 2. const arr = [...Array(10).keys()];
+  // 이렇게 더 간단하게 작성할 수 도 있다.
 
- return answer;   
+  for (let num of arr) {
+    if (!numbers.includes(num)) {
+      answer += num;
+    }
+  }
+
+  return answer;
 }
 
 // 문제 26. 음양 더하기
 // https://school.programmers.co.kr/learn/courses/30/lessons/76501
 // 풀이
 function solution(absolutes, signs) {
-    let answer = 0;
-    for(let i = 0; i < absolutes.length; i++){
-        if(signs[i] === true) {
-            answer += absolutes[i];
-        } else {
-            answer -= absolutes[i];
-        }
+  let answer = 0;
+  for (let i = 0; i < absolutes.length; i++) {
+    if (signs[i] === true) {
+      answer += absolutes[i];
+    } else {
+      answer -= absolutes[i];
     }
-    return answer;
+  }
+  return answer;
 }
 
 // 문제 27. 콜라츠 추측
 // https://school.programmers.co.kr/learn/courses/30/lessons/12943
 // 풀이
 function solution(num) {
-    let count = 0;
-    
-    while(num !== 1){
-        if(num%2 === 0){
-            num = num / 2;
-        } else {
-            num = num * 3 +1;
-        }
-        count ++;
-    }
-    
-    if (count >= 500) {
-        return -1;
+  let count = 0;
+
+  while (num !== 1) {
+    if (num % 2 === 0) {
+      num = num / 2;
     } else {
-        return count;
+      num = num * 3 + 1;
     }
+    count++;
+  }
+
+  if (count >= 500) {
+    return -1;
+  } else {
+    return count;
+  }
+}
+
+// 문제 28. 핸드폰 번호 가리기
+// https://school.programmers.co.kr/learn/courses/30/lessons/12948
+// 풀이 1.
+
+function solution(phone_number) {
+  let answer = [];
+  let starLength = phone_number.length;
+
+  for (let i = 0; i < starLength; i++) {
+    if (i < starLength - 4) {
+      answer.push("*");
+    } else {
+      answer.push(phone_number[i]);
+    }
+  }
+
+  return answer.join("");
+}
+
+// 풀이 2.
+function solution(phone_number) {
+  let result = "*".repeat(phone_number.length - 4) + phone_number.slice(-4);
+  return result;
 }
